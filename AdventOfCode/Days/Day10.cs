5d22d4744832c9212c79d0b90a02cde190ded3af
Bryn THOMAS <Bryn.THOMAS@EDUCATION.GOV.UK>
2021-12-21 22:11:12 +0000

    day 10 start

---
+++ AdventOfCode/Days/Day10.cs
@@ -0,0 +1,29 @@
+﻿namespace AdventOfCode.Days
+{
+    /// <summary>
+    /// </summary>
+    [MemoryDiagnoser]
+    public class Day10
+    {
+        private const string Filename = "Data/Day10/input.txt";
+
+        public IEnumerable<string> Data;
+
+        public Day10(string filename = Filename)
+        {
+            Data = File.ReadLines(filename);
+        }
+
+        [Benchmark]
+        public int Part1()
+        {
+            return 0;
+        }
+
+        [Benchmark]
+        public long Part2()
+        {
+            return 0;
+        }
+    }    
+}
---
+++ Tests/Days/Day10Tests.cs
@@ -0,0 +1,22 @@
+namespace Tests.Days
+{
+    [TestClass]
+    public class Day10Tests
+    {
+        [TestMethod]
+        public void Part1()
+        {
+            var day10 = new Day10();
+            var result = day10.Part1();
+            Assert.AreEqual(26397, result);
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            var day10 = new Day10();
+            var result = day10.Part2();
+            Assert.AreEqual(0, result);
+        }
+    }
+}
